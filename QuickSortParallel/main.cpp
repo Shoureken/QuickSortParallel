d83bad4274a39b874b17cfa8e0528699d1b2cc64
Rodolfo Dematte <rodolfodematte@gmail.com>
2013-04-06 18:50:02 -0300

    Iniciar

---
+++ QuickSortParallel/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: shoureken
+ *
+ * Created on 6 de Abril de 2013, 18:45
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ QuickSortParallel/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=quicksortparallel
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/quicksortparallel
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=quicksortparallel.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/quicksortparallel.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=quicksortparallel
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/quicksortparallel
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=quicksortparallel.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/quicksortparallel.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
