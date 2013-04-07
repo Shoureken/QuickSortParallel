ebe080ec39f064eb0ddc34c7b72f691520d1ffc1
root <root@Rodolfo-Note.(none)>
2013-04-07 05:49:59 -0300

    START inicio da implementação do quicksort

---
+++ QuickSortParallel/quicksort_privado.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   quicksort_private.h
+ * Author: shoureken
+ *
+ * Created on 6 de Abril de 2013, 19:28
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include "quicksort_publico.h"
+#include "ldde_privado.h"
+
+typedef struct {
+    int threadId;
+    int dataSize;
+    pLista lista;
+    Nolista *pivo;
+    pLista maiores;
+    pLista menores;
+    int (*cmp)(void *d1, void *d2);
+    int inicio;
+    int fim;
+} param_t;
